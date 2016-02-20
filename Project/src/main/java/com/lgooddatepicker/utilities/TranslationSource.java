1d96c098648da58d26f08f83a5df699962a3c2ea
BlakeTNC <BlakeTNC@users.noreply.github.com>
2016-02-19 17:34:28 -0800

    Added a translation properties file exception.
    Added a detailed exception to catch any problem loading the translation
    properties file.

--- Project/src/main/java/com/lgooddatepicker/utilities/TranslationSource.java
+++ Project/src/main/java/com/lgooddatepicker/utilities/TranslationSource.java
@@ -17,6 +17,11 @@ public class TranslationSource {
     static private Properties translationResources;
 
     /**
+     * propertiesFileName, This holds the name of the properties file.
+     */
+    static final private String propertiesFileName = "TranslationResources.properties";
+
+    /**
      * getTranslation, This returns a local language translation for the text that is represented by
      * the specified key. The supplied locale is used to indicate the desired language. If a
      * translation cannot be found, then the default text will be returned instead.
@@ -40,12 +45,19 @@ public class TranslationSource {
         if (translationResources != null) {
             return;
         }
+        String lastException = "";
         try {
             translationResources = new Properties();
             ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-            translationResources.load(classLoader.getResourceAsStream("TranslationResources.properties"));
-        } catch (Exception e) {
-            System.out.println(e.toString());
+            translationResources.load(classLoader.getResourceAsStream(
+                    propertiesFileName));
+        } catch (Exception exception) {
+            lastException = exception.toString() + "\n----\n" + exception.getMessage();
+        }
+        if (translationResources == null) {
+            throw new RuntimeException("TranslationSource.initializePropertiesIfNeeded()"
+                    + "Could not load TranslationResources.properties file.\n" +
+                    "The exception follows:\n"+ lastException);
         }
     }
 
