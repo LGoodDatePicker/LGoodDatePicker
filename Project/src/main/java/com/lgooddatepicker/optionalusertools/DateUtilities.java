b80ae6add976a9d1427f273d1c5b5d21a6ff01da
BlakeTNC <BlakeTNC@users.noreply.github.com>
2016-02-23 13:14:13 -0800

    Fixed bug in function DateUtilities.isSameLocalDate().
    Fixed a bug in the implementation of DateUtilities.isSameLocalDate().

--- Project/src/main/java/com/lgooddatepicker/optionalusertools/DateUtilities.java
+++ Project/src/main/java/com/lgooddatepicker/optionalusertools/DateUtilities.java
@@ -9,16 +9,23 @@ import java.time.LocalDate;
 public class DateUtilities {
 
     /**
-     * isSameLocalDate, Compares two date variables to see if their values are equal. Returns true
-     * if they are equal, false if they are not equal.
+     * isSameLocalDate, This compares two date variables to see if their values are equal. Returns
+     * true if the values are equal, otherwise returns false.
      *
-     * More specifically: This returns true if both of the supplied dates represent the same date.
-     * This returns true if both values are null (an empty date). Otherwise this returns false.
+     * More specifically: This returns true if both values are null (an empty date). Or, this
+     * returns true if both of the supplied dates contain a date and represent the same date.
+     * Otherwise this returns false.
      */
     static public boolean isSameLocalDate(LocalDate first, LocalDate second) {
-        if (first == null) {
-            return (second == null);
+        // If both values are null, return true.
+        if (first == null && second == null) {
+            return true;
         }
+        // At least one value contains a date. If the other value is null, then return false.
+        if (first == null || second == null) {
+            return false;
+        }
+        // Both values contain dates. Return true if the dates are equal, otherwise return false.
         return first.isEqual(second);
     }
 }
