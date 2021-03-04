3f2af14815d219bc142ae89a53691084808f913b
WiseEagleOwl <53954419+WiseEagleOwl@users.noreply.github.com>
2021-03-04 22:23:56 +0100

    Move tests from default package into lgooddatepicker package

--- Project/src/test/java/com/github/lgooddatepicker/TestRequirements.java
+++ Project/src/test/java/com/github/lgooddatepicker/TestRequirements.java
@@ -20,6 +20,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+package com.github.lgooddatepicker;
 
 import com.github.lgooddatepicker.components.CalendarPanel;
 import com.github.lgooddatepicker.components.DatePicker;
