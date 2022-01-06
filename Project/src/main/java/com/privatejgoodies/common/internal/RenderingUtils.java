706ace5ea58c44e8400ff89d829e4b3821e56810
David B Malkovsky <david@malkovsky.org>
2022-01-06 17:08:41 -0500

    Chore: remove redundant array creation for varargs (#169)
    * Chore: remove redundant array creation for varargs
    
    * Google Java Format
    
    Co-authored-by: github-actions <>

--- Project/src/main/java/com/privatejgoodies/common/internal/RenderingUtils.java
+++ Project/src/main/java/com/privatejgoodies/common/internal/RenderingUtils.java
@@ -118,8 +118,7 @@ public final class RenderingUtils {
       JComponent c, Graphics g, String text, int underlinedIndex, int x, int y) {
     if (drawStringUnderlineCharAtMethod != null) {
       try {
-        drawStringUnderlineCharAtMethod.invoke(
-            null, new Object[] {c, g, text, underlinedIndex, x, y});
+        drawStringUnderlineCharAtMethod.invoke(null, c, g, text, underlinedIndex, x, y);
         return;
       } catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
         // Use the BasicGraphicsUtils as fallback
@@ -163,8 +162,7 @@ public final class RenderingUtils {
     try {
       Class<?> clazz = Class.forName(SWING_UTILITIES2_NAME);
       return clazz.getMethod(
-          "drawString",
-          new Class[] {JComponent.class, Graphics.class, String.class, Integer.TYPE, Integer.TYPE});
+          "drawString", JComponent.class, Graphics.class, String.class, Integer.TYPE, Integer.TYPE);
     } catch (ClassNotFoundException | SecurityException | NoSuchMethodException e) {
       // returns null
     }
@@ -176,9 +174,12 @@ public final class RenderingUtils {
       Class<?> clazz = Class.forName(SWING_UTILITIES2_NAME);
       return clazz.getMethod(
           "drawStringUnderlineCharAt",
-          new Class[] {
-            JComponent.class, Graphics.class, String.class, Integer.TYPE, Integer.TYPE, Integer.TYPE
-          });
+          JComponent.class,
+          Graphics.class,
+          String.class,
+          Integer.TYPE,
+          Integer.TYPE,
+          Integer.TYPE);
     } catch (ClassNotFoundException | SecurityException | NoSuchMethodException e) {
       // returns null
     }
@@ -188,7 +189,7 @@ public final class RenderingUtils {
   private static Method getMethodGetFontMetrics() {
     try {
       Class<?> clazz = Class.forName(SWING_UTILITIES2_NAME);
-      return clazz.getMethod("getFontMetrics", new Class[] {JComponent.class, Graphics.class});
+      return clazz.getMethod("getFontMetrics", JComponent.class, Graphics.class);
     } catch (ClassNotFoundException | SecurityException | NoSuchMethodException e) {
       // returns null
     }
